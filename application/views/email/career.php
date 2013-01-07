c74d5374c5bddb75a56e7a848568ddf62c1aaca0
unknown <aman>
2013-01-07 16:17:53 +0700

    career

--- application/views/email/career.php
+++ application/views/email/career.php
@@ -1,37 +1,53 @@
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-</head>
-
-<body>
-    <form action="" method="post" enctype="multipart/form-data" name="form1" id="form1">
-        <p>
-            <label>First name
-                <input type="text" name="First name" id="First name" />
-            </label>
-        </p>
-        <p>&nbsp;</p>
-        <p>
-            <label>Last name
-                <input type="text" name="Last name" id="Last name" />
-            </label>
-        </p>
-        <p>&nbsp;</p>
-        <p>
-            <label>Phone no
-                <input type="text" name="Phone no" id="Phone no" />
-            </label>
-        </p>
-        <p>&nbsp;</p>
-        <p>
-            <label>File Upload
-                <br />
-                <br />
-                <input type="file" name="fileField" id="fileField" />
-            </label>
-        </p>
-        <p>
-            <input type="submit" name="Submit" id="button" value="Submit" />
-        </p>
-    </form>
-</body>
-</html>
+<form action="" method="post" enctype="multipart/form-data" name="form1">
+    <label>
+        <ul>
+            <li>
+                <h4><strong>First name      </strong></h4>
+            </li>
+        </ul>
+        <blockquote>
+            <blockquote>
+                <p>
+                    <input type="text" name="First name" id="First name">
+                </p>
+            </blockquote>
+        </blockquote>
+        <ul>
+            <li>
+                <h4><strong>
+                        Last name</strong></h4>
+            </li>
+            <blockquote>
+                <p>
+                    <input name="Last name" type="text" id="Last name" value="">
+                </p>
+            </blockquote>
+            <li>
+                <h4><strong>Phone no        </strong></h4>
+            </li>
+            <blockquote>
+                <p>
+                    <input type="text" name="Phone no" id="Phone no">
+                </p>
+            </blockquote>
+            <li>
+                <h4><strong>File Upload </strong></h4>
+            </li>
+            <blockquote>
+                <p>
+                    <input type="file" name="fileField" id="fileField">
+                </p>
+                <p>
+                    <input type="submit" name="button" id="button" value="Submit">
+                </p>
+                <ul>
+                    <blockquote>
+                        <p>&nbsp;</p>
+                    </blockquote>
+                    <ul>
+                    </ul>
+                </ul>
+            </blockquote>
+        </ul>
+    </label>
+</form>
