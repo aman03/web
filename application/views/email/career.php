35c1e81797d1d0ab7774182e80ea64e13eeb1d14
unknown <aman>
2013-01-08 20:45:10 +0700

    update

--- application/views/email/career.php
+++ application/views/email/career.php
@@ -1,53 +1,13 @@
-<form action="" method="post" enctype="multipart/form-data" name="form1">
-    <label>
-        <ul>
-            <li>
-                <h4><strong>First name      </strong></h4>
-            </li>
-        </ul>
-        <blockquote>
-            <blockquote>
-                <p>
-                    <input type="text" name="First name" id="First name">
-                </p>
-            </blockquote>
-        </blockquote>
-        <ul>
-            <li>
-                <h4><strong>
-                        Last name</strong></h4>
-            </li>
-            <blockquote>
-                <p>
-                    <input name="Last name" type="text" id="Last name" value="">
-                </p>
-            </blockquote>
-            <li>
-                <h4><strong>Phone no        </strong></h4>
-            </li>
-            <blockquote>
-                <p>
-                    <input type="text" name="Phone no" id="Phone no">
-                </p>
-            </blockquote>
-            <li>
-                <h4><strong>File Upload </strong></h4>
-            </li>
-            <blockquote>
-                <p>
-                    <input type="file" name="fileField" id="fileField">
-                </p>
-                <p>
-                    <input type="submit" name="button" id="button" value="Submit">
-                </p>
-                <ul>
-                    <blockquote>
-                        <p>&nbsp;</p>
-                    </blockquote>
-                    <ul>
-                    </ul>
-                </ul>
-            </blockquote>
-        </ul>
-    </label>
+
+<form action="" method="post">
+ <table>
+
+     <tr><td>First name:</td><td><input type="text" name="fname" value="" /></td></tr>
+      <tr><td>Last name :</td><td><input type="text" name="lname" value="" /></td></tr>
+       <tr><td>Phone no::</td><td><input type="text" name="phone no:" value="" /></td></tr>
+       <tr><td>Attach File</td><td>
+        <input type="file" name="file_upload" id="file" /> </td></tr>
+    <tr><td></td><td><input type="submit" name="send" value="Send" /></td></tr>
+    </table>
+
 </form>
