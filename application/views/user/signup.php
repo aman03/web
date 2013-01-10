553c5d269e41dbda2219dfcdcb7a7afdd417b0c6
Amandeep Singh <aman_sonu03@yahoo.com>
2013-01-10 19:25:01 +0700

    hello

--- application/views/user/signup.php
+++ application/views/user/signup.php
@@ -1,20 +1,20 @@
 
 <form action="" method="post">
-  <fieldset class="Sign Up">
-    <h2> Sign Up</h2>
-    <table> 
-      <tr><td>First name:</td><td><input type="text" name="fname" value="" /></td></tr>
-      <tr><td>Last name:</td><td><input type="text" name="lname" value="" /></td></tr>
-      <tr><td>Username :</td><td><input type="text" name="username" value="" /></td></tr>
-      <tr><td>Password :</td><td><input type="password" name="password:" value="" /></td></tr>
-      <tr><td>Confirm Password :</td><td><input type="cpassword" name="cpassword" value="" /></td></tr>
-      <tr><td>Gender </td>
-        <td><input type="radio" value="male" name="gender" checked="checked" />
-        <label class="choice" >Male</label>
-        <input type="radio" value="female" name="gender" />
-        <label class="choice" >Female</label></td></tr>
-      <tr><td>Phone no::</td><td><input type="text" name="phone no:" value="" /></td></tr>
-      <tr><td></td><td><input type="submit" name="submit" value="submit" /></td></tr>
-    </table>
-  </fieldset>
+    <fieldset class="Sign Up">
+        <h2> Sign Up</h2>
+        <table> 
+            <tr><td>First name:</td><td><input type="text" name="fname" value="" /></td></tr>
+            <tr><td>Last name:</td><td><input type="text" name="lname" value="" /></td></tr>
+            <tr><td>Username :</td><td><input type="text" name="username" value="" /></td></tr>
+            <tr><td>Password :</td><td><input type="password" name="password:" value="" /></td></tr>
+            <tr><td>Confirm Password :</td><td><input type="cpassword" name="cpassword" value="" /></td></tr>
+            <tr><td>Gender </td>
+                <td><input type="radio" value="male" name="gender" checked="checked" />
+                    <label class="choice" >Male</label>
+                    <input type="radio" value="female" name="gender" />
+                    <label class="choice" >Female</label></td></tr>
+            <tr><td>Phone no::</td><td><input type="text" name="phone no:" value="" /></td></tr>
+            <tr><td></td><td><input type="submit" name="submit" value="submit" /></td></tr>
+        </table>
+    </fieldset>
 </form>
